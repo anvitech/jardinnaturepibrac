f2aad6a796e024388d52c02cbc9c557c96ddb22a
Viet Tran <hungviet.tran@gmail.com>
2025-09-05 09:05:15 +0200

    Setting internationalization

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="absolute top-0 bg-opacity-0 row-start-1 flex items-center justify-between w-full bg-opacity-0">
       <div className="flex lg:flex-1">
-        <a href="#" className="-m-1.5 p-1.5">
+        <a href="/" className="-m-1.5 p-1.5">
           <span className="sr-only">Your Company</span>
           <img src="images/logos/logo_JNP_100_200.gif" alt="" className="h-10 w-auto px-6" />
         </a>
--- src/app/ui/navigation_bar.tsx
+++ src/app/ui/navigation_bar.tsx
@@ -1,17 +1,19 @@
+import {useTranslations} from 'next-intl';
 
 const menuItems = [
-  { name: "Association", href: "#home" },
-  { name: "Agenda", href: "#agenda" },
-  { name: "Forum", href: "#forum" },
-  { name: "Chemin de la biodiversité", href: "#chemin_biodiversite" },
+  { name: "association", href: "#home" },
+  { name: "agenda", href: "#agenda" },
+  { name: "forum", href: "#forum" },
+  { name: "biodiversity_path", href: "#chemin_biodiversite" },
 ];
 
 export default function NavigationBar() {
+  const t = useTranslations('Menus');
   return (
     <nav aria-label="navigation" className="flex items-center justify-between p-6 lg:px-8 text-green-700 text-2xl font-semibold">
       <ul className="flex gap-[16px]">
         {menuItems.map((item) => (
-          <li key={item.name}><a href={item.href}>{item.name}</a></li>
+          <li key={item.name}><a href={item.href}>{t(item.name)}</a></li>
         ))}
       </ul>
     </nav>
