c84f878241a6f3b87a809d7286cf40050a207c5d
Viet Tran <hungviet.tran@gmail.com>
2025-09-08 22:31:59 +0200

    Rename footer, header, hero

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import {Locale} from 'next-intl';
 import {setRequestLocale} from 'next-intl/server';
 import {use} from 'react';
 import PageLayout from '@/app/ui/PageLayout';
-import HeroSection from '@/app/ui/Hero';
+import HeroSection from '@/app/ui/hero';
 import ActivitiesSection from '../ui/sections/ActivitiesSection';
 import AppointmentSection from '../ui/sections/AppointmentSection';
 import ContactSection from '../ui/sections/ContactSection';
