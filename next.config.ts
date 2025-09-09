13aeeecfedd9508c7577e94dfa7fff3680b15e0f
Viet Tran <hungviet.tran@gmail.com>
2025-09-09 10:52:48 +0200

    Add basePath variable

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,18 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 
+const isProduction = process.env.NODE_ENV === 'production'
+
+export const basePath = isProduction ? '/jardinnaturepibrac' : '';
+
 const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   distDir: "dist",
   images: { unoptimized: true },
-  basePath: '/jardinnaturepibrac',
+  assetPrefix: isProduction ? basePath : '',
+  basePath: isProduction ? basePath : '',
 };
 
 export default withNextIntl(nextConfig);
--- src/app/ui/ButtonImage.tsx
+++ src/app/ui/ButtonImage.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { basePath } from '../../../next.config';
 
 type ButtonImageProps = {
   href: string;
@@ -14,11 +15,12 @@ export default function ButtonImage({
   src, 
   alt,
   width = 200,
+  height = 64,
 }: Readonly<ButtonImageProps>) {
   const img_classname = `m-auto max-h-full justify-center`;
   return (
     <Link href={href} className="p-0 border-0 bg-transparent cursor-pointer justify-center flex">
-      <Image src={src} alt={alt} className={img_classname} width={width} height={64}/>
+      <Image src={`${basePath}${src}`} alt={alt} className={img_classname} width={width} height={height}/>
     </Link>
   );
 }
