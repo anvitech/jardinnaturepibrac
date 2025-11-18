c6a8defff7b36a6a6373f65b841984f6cae67727
Viet Tran <hungviet.tran@gmail.com>
2025-11-18 23:56:18 +0100

    fix: use iframe for display Google Map card

--- src/app/ui/GoogleMapCard.tsx
+++ src/app/ui/GoogleMapCard.tsx
@@ -1,15 +1,15 @@
-import { GoogleMapsEmbed } from '@next/third-parties/google';
-
 export default function GoogleMapCard() {
   return (
-    <div className="w-full flex justify-center">
-      <GoogleMapsEmbed
-        apiKey="AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg"
-        height={400}
-        width={600}
-        mode="place"
-        q="Salle Polyvalente 6 boulevard des écoles, 31820 Pibrac, France"
-      />
+    <div className="w-full h-96 flex justify-center">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d645.0065407484609!2d1.2839408019839782!3d43.618496062189145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aeb21b79ae85a7%3A0xc640573520b05009!2sEsp.%20Sainte-Germaine%2C%2031820%20Pibrac!5e1!3m2!1sfr!2sfr!4v1760861252823!5m2!1sfr!2sfr"
+        width="100%"
+        height="100%"
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        title="Google Map"
+      >
+      </iframe>
     </div>
   );
 }
